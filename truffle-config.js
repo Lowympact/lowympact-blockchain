4a2bcebbfa50f7ccd53ce4dbcffcbec1582ab5ef
lperrious <louis30@me.com>
2021-04-24 16:20:51 +0200

    init: add new VPS network

--- truffle-config.js
+++ truffle-config.js
@@ -47,6 +47,11 @@ module.exports = {
       port: 7545, // Standard Ethereum port (default: none)
       network_id: "*", // Any network (default: none)
     },
+    production: {
+      host: "",
+      port: 7454,
+      network_id: "*",
+    },
     // Another network with more advanced options...
     // advanced: {
     // port: 8777,             // Custom port
