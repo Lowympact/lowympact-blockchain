a85e40fbc18a46cf3fa1d6c650a8539d7333ca93
lperrious <louis30@me.com>
2021-04-27 11:51:00 +0200

    feat: implement Actor and Transaction

---
+++ contracts/Lowympact.sol
@@ -0,0 +1,103 @@
+pragma solidity >=0.4.22 <0.9.0;
+
+pragma experimental ABIEncoderV2;
+
+contract Actor {
+    string public id;
+    string public name;
+    string public actorType;
+    string public latitude;
+    string public longitude;
+    address public owner;
+
+    constructor(
+        string memory _id,
+        string memory _name,
+        string memory _actorType,
+        string memory _latitude,
+        string memory _longitude
+    ) public {
+        id = _id;
+        name = _name;
+        actorType = _actorType;
+        latitude = _latitude;
+        longitude = _longitude;
+        owner = msg.sender;
+    }
+
+    function createTransaction(
+        Transaction.Product[] memory _productsInput,
+        Transaction.Product[] memory _productsOutput,
+        Actor _buyer,
+        string memory _idTransaction,
+        Transaction.TransportType _transport
+    ) public returns (Transaction newTransaction) {
+        require(msg.sender == owner, "Only the actor can create a transaction");
+
+        return
+            new Transaction(
+                _idTransaction,
+                _buyer,
+                _transport,
+                _productsInput,
+                _productsOutput
+            );
+    }
+}
+
+contract Transaction {
+    enum TransportType {Avion, Train, Bateau, Camion, Charette}
+
+    struct Product {
+        uint256 productsInputId;
+        address addressTransaction;
+    }
+
+    struct Products {
+        mapping(uint256 => Product) products;
+        uint256 size;
+    }
+
+    string public idTransaction;
+    Actor public buyer;
+    Actor public seller;
+    uint256 public date;
+    TransportType public transport;
+    Product[] public productsInput;
+    Product[] public productsOutput;
+    bool public isFinished;
+    bool public isAccepted;
+
+    constructor(
+        string memory _idTransaction,
+        Actor _buyer,
+        TransportType _transport,
+        Product[] memory _productsInput,
+        Product[] memory _productsOutput
+    ) public {
+        idTransaction = _idTransaction;
+        buyer = _buyer;
+        date = block.timestamp;
+        transport = _transport;
+        for (uint256 index = 0; index < _productsInput.length; index++) {
+            productsInput.push(_productsOutput[index]);
+        }
+        for (uint256 index = 0; index < _productsOutput.length; index++) {
+            productsOutput.push(_productsOutput[index]);
+        }
+        isFinished = false;
+        isAccepted = false;
+    }
+
+    function acceptTransaction() external {
+        if (msg.sender == buyer.owner()) {
+            isAccepted = true;
+        }
+    }
+
+    function finishTransaction() external {
+        if (msg.sender == buyer.owner()) {
+            isFinished = true;
+        }
+    }
+}
